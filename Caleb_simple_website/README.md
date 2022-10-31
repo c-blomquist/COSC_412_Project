fe9345587d8cf806355e2da5bcfb9aa3a321cea9
Caleb Blomquist <91571032+c-blomquist@users.noreply.github.com>
2022-10-31 12:59:58 -0400

    Example of HTML, CSS, and Javascript

---
+++ Caleb_simple_website/README.md
@@ -0,0 +1,7 @@
+#Example  
+
+This is just a small example of a working website using HTML, CSS, and Javascript to create an interactable frontend.  
+This will be expanded upon to create the actual website with links to other pages, like a login page.  
+
+
+The resource used for this small example is here: https://learn.microsoft.com/en-us/training/modules/build-simple-website/1-introduction
---
+++ Caleb_simple_website/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const switcher = document.querySelector('.btn'); //This creates a reference to the .btn in the css file which links to the class in HTML
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+});
